1dc6811a4aeb8672aa130b3c8c85a196b8d7814d
Prageeth Karunadheera <prageeth@lotj.com>
2022-10-20 14:49:52 +1100

    include missing action->forward

--- alb-target-group.tf
+++ alb-target-group.tf
@@ -1,91 +1,79 @@
 resource "aws_lb_listener_rule" "green" {
-  listener_arn = var.alb_listener_https_arn
+  listener_arn = "${var.alb_listener_https_arn}"
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.green.arn
-  }
-
-  dynamic "condition" {
-    for_each = length(var.paths) > 0 ? [var.paths] : []
-    content {
-      path_pattern {
-        values = toset(condition.value)
+    target_group_arn = "${aws_lb_target_group.green.arn}"
+    forward {
+      stickiness {
+        duration = 0
+        enabled  = false
+      }
+      target_group {
+        arn    = aws_lb_target_group.api_target_group_blue.arn
+        weight = 100
+      }
+      target_group {
+        arn    = aws_lb_target_group.api_target_group_green.arn
+        weight = 0
       }
     }
   }
 
-  dynamic "condition" {
-    for_each = length(var.hostnames) > 0 ? [var.hostnames] : []
-    content {
-      host_header {
-        values = toset(condition.value)
-      }
-    }
-  }
-
-  dynamic "condition" {
-    for_each = length(var.source_ips) > 0 ? [var.source_ips] : []
-    content {
-      source_ip {
-        values = toset(condition.value)
-      }
-    }
+  condition {
+    field  = "host-header"
+    values = split(",", var.hostname)
   }
 
   lifecycle {
     ignore_changes = [
-      action[0].target_group_arn
+      action[0].target_group_arn,
+      condition,
     ]
   }
-
-  priority = var.alb_priority != 0 ? var.alb_priority : null
 }
 
 resource "aws_lb_listener_rule" "blue" {
-  listener_arn = var.test_traffic_route_listener_arn
+  listener_arn = "${var.alb_listener_https_arn}"
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
-  }
-
-  dynamic "condition" {
-    for_each = length(var.paths) > 0 ? [var.paths] : []
-    content {
-      path_pattern {
-        values = toset(condition.value)
+    target_group_arn = "${aws_lb_target_group.blue.arn}"
+    forward {
+      stickiness {
+        duration = 0
+        enabled  = false
+      }
+      target_group {
+        arn    = aws_lb_target_group.api_target_group_blue.arn
+        weight = 100
+      }
+      target_group {
+        arn    = aws_lb_target_group.api_target_group_green.arn
+        weight = 0
       }
     }
   }
 
-  dynamic "condition" {
-    for_each = length(var.hostnames) > 0 ? [var.hostnames] : []
-    content {
-      host_header {
-        values = toset(condition.value)
-      }
-    }
+  condition {
+    field  = "host-header"
+    values = ["${var.hostname_blue}"]
   }
 
   lifecycle {
-    ignore_changes = [
-      action[0].target_group_arn
-    ]
+    ignore_changes = ["action[0].target_group_arn"]
   }
-
-  priority = var.alb_priority != 0 ? var.alb_priority + 1 : null
 }
 
 resource "aws_lb_listener_rule" "redirects" {
-  count        = length(compact(split(",", var.hostname_redirects)))
-  listener_arn = var.alb_listener_https_arn
+  count        = "${length(compact(split(",", var.hostname_redirects)))}"
+  listener_arn = "${var.alb_listener_https_arn}"
 
   action {
     type = "redirect"
 
     redirect {
-      host        = var.hostnames[0]
+      host        = "${var.hostname}"
       port        = "443"
       protocol    = "HTTPS"
       status_code = "HTTP_301"
@@ -93,53 +81,33 @@ resource "aws_lb_listener_rule" "redirects" {
   }
 
   condition {
-    host_header {
-      values = [element(split(",", var.hostname_redirects), count.index)]
-    }
+    field  = "host-header"
+    values = ["${element(split(",", var.hostname_redirects), count.index)}"]
   }
 }
 
-# Generate a random string to add it to the name of the Target Group
-resource "random_string" "alb_prefix" {
-  length  = 4
-  upper   = false
-  special = false
-}
-
 resource "aws_lb_target_group" "green" {
-  name                 = var.compat_keep_target_group_naming ? "${var.cluster_name}-${var.name}-gr" : format("%s-gr-%s", substr("${var.cluster_name}-${var.name}", 0, 24), random_string.alb_prefix.result)
-  port                 = var.port
-  protocol             = var.protocol
-  vpc_id               = var.vpc_id
+  name                 = "ecs-${var.name}-green"
+  port                 = "${var.port}"
+  protocol             = "HTTP"
+  vpc_id               = "${var.vpc_id}"
   deregistration_delay = 10
-  target_type          = var.launch_type == "FARGATE" ? "ip" : "instance"
 
   health_check {
-    path                = var.healthcheck_path
-    interval            = var.healthcheck_interval
-    healthy_threshold   = var.healthy_threshold
-    unhealthy_threshold = var.unhealthy_threshold
-    timeout             = var.healthcheck_timeout
-    matcher             = var.healthcheck_matcher
-    protocol            = var.protocol
+    path     = "${var.healthcheck_path}"
+    interval = "${var.healthcheck_interval}"
   }
 }
 
 resource "aws_lb_target_group" "blue" {
-  name                 = var.compat_keep_target_group_naming ? "${var.cluster_name}-${var.name}-bl" : format("%s-bl-%s", substr("${var.cluster_name}-${var.name}", 0, 24), random_string.alb_prefix.result)
-  port                 = var.port
-  protocol             = var.protocol
-  vpc_id               = var.vpc_id
+  name                 = "ecs-${var.name}-blue"
+  port                 = "${var.port}"
+  protocol             = "HTTP"
+  vpc_id               = "${var.vpc_id}"
   deregistration_delay = 10
-  target_type          = var.launch_type == "FARGATE" ? "ip" : "instance"
 
   health_check {
-    path                = var.healthcheck_path
-    interval            = var.healthcheck_interval
-    healthy_threshold   = var.healthy_threshold
-    unhealthy_threshold = var.unhealthy_threshold
-    timeout             = var.healthcheck_timeout
-    matcher             = var.healthcheck_matcher
-    protocol            = var.protocol
+    path     = "${var.healthcheck_path}"
+    interval = "${var.healthcheck_interval}"
   }
 }
